059ca5f1db5330a74b4073a62b17152cd681dcfc
sohaghosen <sohag.hosen@outlook.com>
2022-04-14 00:38:51 +0600

    update theme

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -20,14 +20,14 @@ const getDesignTokens = (mode) => ({
           // palette values for light mode
           primary: {
             // light: will be calculated from palette.primary.main,
-            main: blueGrey[900],
+            main: blueGrey[50],
             // dark: will be calculated from palette.primary.main,
             // contrastText: will be calculated to contrast with palette.primary.main
           },
           // divider: blueGrey[200],
           background: {
-            default: blueGrey[900],
-            paper: blueGrey[900],
+            default: blueGrey[50],
+            paper: blueGrey[50],
           },
           text: {
             primary: blueGrey[900],
@@ -37,15 +37,15 @@ const getDesignTokens = (mode) => ({
       : {
           // palette values for dark mode
           primary: {
-            main: red[900],
+            main: blueGrey[900],
           },
           background: {
-            default: yellow[900],
-            paper: yellow[900],
+            default: blueGrey[900],
+            paper: blueGrey[900],
           },
           text: {
-            primary: purple[900],
-            secondary: indigo[900],
+            primary: blueGrey[50],
+            secondary: blueGrey[50],
           },
         }),
   },
